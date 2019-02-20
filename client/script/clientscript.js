d79e713def8dbc17a990fd3d43acd238c12acb37
minhhehe <37853220+minhhehe@users.noreply.github.com>
2019-02-20 11:49:10 -0700

    fetched data successfully, need to format into... formats...

--- client/script/clientscript.js
+++ client/script/clientscript.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
 
   $("#task11Button").on("click", function(e) {
     e.preventDefault();
-    var school_text = $('#task5Value').serialize();
+    var school_text = $('#task1Value').val();
     var type_selected = $('#typeListID option:selected').text();
     $.ajax({
-        url: 'http://localhost:80/server/task11.php', // point to server-side PHP script
-        dataType: 'text',  // what to expect back from the PHP script, if anything
-        cache: false,
-        contentType: false,
-        processData: false,
-        data: school_text,
-        type: 'POST',
-        success: function(php_script_response){
-          $('#resultSpace11').html(php_script_response);
-        }
+      url: 'http://localhost:80/server/task11.php', // point to server-side PHP script
+      //dataType: 'text',  // what to expect back from the PHP script, if anything
+      data: {
+        school_data: school_text,
+        type_data: type_selected
+      },
+      type: 'post',
+      success: function(php_script_response){
+        $('#resultSpace11').html(php_script_response);
+      }
      });
      return false;
   });
--- server/task11.php
+++ server/task11.php
@@ -2,15 +2,44 @@
 <body>
 
   <?php
-    var_dump($_POST);
-    var_dump($_GET);
-    var_dump($_REQUEST);
-    $input_school = $_POST["school_data"];
-    $input_type = $_POST['type_data'];
-    echo $input_type;
-    echo "<br>";
-    echo $input_school;
-   ?>
+
+    $host='localhost';
+    $db = 'SENG401'; //use pgadmin to create a database e.g. SENG401
+    $username = 'postgres'; //usually postgres
+    $password = "Estelle@88938."; //usually postgres
+    $port = 5432;
+    $dsn = "pgsql:host=$host; port=$port; dbname=$db; user=$username;
+    password=$password";
+    try{
+      $input_school = $_POST["school_data"];
+      $input_type = $_POST['type_data'];
+      echo $input_type;
+      //JSON
+      echo "<br>";
+      echo $input_school;
+      //value= zzzz
+      echo "<br>";
+      // create a PostgreSQL database connection
+      $conn = new PDO($dsn);
+      // display a message if connected to the PostgreSQL successfully
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      // set the PDO error mode to Exception
+      //prepare the statement
+      $statement = $conn->prepare("SELECT * FROM CalgarySchools
+              WHERE NAME LIKE :input");
+      $tosearch = "%$input_school%";
+      $statement->bindParam(":input", $tosearch);
+      $statement->execute();
+      $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
+      $result2 = $statement->fetchAll();
+      echo "<br>";
+      echo $result;
+      echo "<br>";
+      } catch (PDOException $e){
+      // report error message
+      echo $e->getMessage();
+    }
+    ?>
 
 </body>
 </html>
