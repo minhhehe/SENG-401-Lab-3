faf2a19f87d9d29f3ba1f6ed421aefe6d35847cf
R Lee <alpha.r.lee.dev@gmail.com>
2019-02-25 16:33:29 -0700

    Added get schools by sector Debugged findSchools

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include "dbConnector.php";
-$inputName = $inputFormat = $inputSector = "";
+$inputName = $inputFormatByName = $inputFormatBySector = $inputSector = "";
 $output = "";
 $errorName = "";
 
@@ -8,19 +8,9 @@ function validateInput($input) {
     return htmlspecialchars(stripslashes(trim($input)));
 }
 
-function findSchool($name, $format) {
-    $table = "CalgarySchools";
-    $nameCol = "name";
-
-    $query = $GLOBALS['conn']->prepare("SELECT $nameCol FROM $table WHERE $nameCol LIKE :name");
-    $query->bindValue(":name", "%$name%");
-    $query->execute();
-    $results = $query->fetchAll(PDO::FETCH_ASSOC); // Fetch all results with only associative indexing
-    $GLOBALS['output'] = getOutput($results, $format);
-}
-
 /**
- * Get an
+ * Get an output based on the desired input format
+ * Returns a string representation of the output. Double-spaces and newlines are replaced with their HTML equivalents
  */
 function getOutput($queryResults, $format) {
     switch ($format) {
@@ -109,6 +99,36 @@ function getTableOutput($queryResults) {
     return $output;
 }
 
+function findSchool($name, $format) {
+    $table = "CalgarySchools";
+    $nameCol = "name";
+    $typeCol = "type";
+    $addressCol = "address";
+
+    $query = $GLOBALS["conn"]->prepare("SELECT $nameCol, $typeCol, $addressCol FROM $table WHERE $nameCol LIKE :name");
+    $query->bindValue(":name", "%$name%");
+    $query->execute();
+    $results = $query->fetchAll(PDO::FETCH_ASSOC); // Fetch all results with only associative indexing
+    $GLOBALS["output"] = getOutput($results, $format);
+}
+
+function getSectorSummary($sector, $format) {
+    $table = "CalgarySchools";
+    $nameCol = "name";
+    $typeCol = "type";
+    $sectorCol = "sector";
+    $addressCol = "address";
+
+    $query = $GLOBALS["conn"]->prepare("SELECT $typeCol AS 'Type', COUNT($typeCol) AS 'Count'
+            FROM $table
+            WHERE $sectorCol = :sector
+            GROUP BY $typeCol");
+    $query->bindValue(":sector", $sector);
+    $query->execute();
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    $GLOBALS["output"] = getOutput($results, $format);
+}
 
 if ($conn) {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -119,12 +139,16 @@ if ($conn) {
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     if (isset($_GET["submitFindSchool"])) {
         if (!empty($_GET["inputName"])) {
-            $inputName = validateInput($_GET['inputName']);
-            $inputFormat = validateInput($_GET['inputFormat']);
-            findSchool($inputName, $inputFormat);
+            $inputName = validateInput($_GET["inputName"]);
+            $inputFormatByName = validateInput($_GET["inputFormatByName"]);
+            findSchool($inputName, $inputFormatByName);
         } else {
             $errorName = "* Required";
         }
+    } else if (!empty($_GET["submitSectorSummary"])) {
+        $inputSector = validateInput($_GET["inputSector"]);
+        $inputFormatBySector = validateInput($_GET["inputFormatBySector"]);
+        getSectorSummary($inputSector, $inputFormatBySector);
     }
 }
 ?>
@@ -160,24 +184,68 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
                             <span class="error"><?php echo $errorName; ?></span>
                         </p>
 
-                        <input type="radio" name="inputFormat" value="json"
-                        <?php echo ($inputFormat == "json" || $inputFormat == "") ? 'checked="checked"' : ''; // Save last checked or default if none given ?>
+                        <input type="radio" name="inputFormatByName" value="json"
+                        <?php echo ($inputFormatByName == "json" || $inputFormatByName == "") ? 'checked="checked"' : ''; // Save last checked or default if none given ?>
                         >JSON<br/>
 
-                        <input type="radio" name="inputFormat" value="xml"
-                        <?php echo ($inputFormat == "xml") ? 'checked="checked"' : ''; // Save last checked ?>
+                        <input type="radio" name="inputFormatByName" value="xml"
+                        <?php echo ($inputFormatByName == "xml") ? 'checked="checked"' : ''; // Save last checked ?>
                         >XML<br/>
 
-                        <input type="radio" name="inputFormat" value="csv"
-                        <?php echo ($inputFormat == "csv") ? 'checked="checked"' : ''; // Save last checked ?>
+                        <input type="radio" name="inputFormatByName" value="csv"
+                        <?php echo ($inputFormatByName == "csv") ? 'checked="checked"' : ''; // Save last checked ?>
                         >CSV<br/>
 
-                        <input type="radio" name="inputFormat" value="table"
-                        <?php echo ($inputFormat == "table") ? 'checked="checked"' : ''; // Save last checked ?>
+                        <input type="radio" name="inputFormatByName" value="table"
+                        <?php echo ($inputFormatByName == "table") ? 'checked="checked"' : ''; // Save last checked ?>
                         >Table<br/>
                         <br/>
 
                         <input type="submit" name="submitFindSchool" value="Submit">
+                    </form>
+                </div>
+                <br>
+                <div class="inputBlock">
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="get">
+                        <h3>Get Summary by Sector</h3>
+                        <p>
+                            Sector:
+                            <select name="inputSector">
+                                <option value="nw"
+                                <?php echo ($inputSector == "nw" || $inputSector == "") ? 'selected="selected"' : ''; // Save last checked ?>
+                                >NW</option>
+
+                                <option value="ne"
+                                <?php echo ($inputSector == "ne") ? 'selected="selected"' : ''; // Save last checked ?>
+                                >NE</option>
+
+                                <option value="sw"
+                                <?php echo ($inputSector == "sw") ? 'selected="selected"' : ''; // Save last checked ?>
+                                >SW</option>
+
+                                <option value="se"
+                                <?php echo ($inputSector == "se") ? 'selected="selected"' : ''; // Save last checked ?>
+                                >SE</option>
+                            </select>
+                        </p>
+
+                        <input type="radio" name="inputFormatBySector" value="json"
+                        <?php echo ($inputFormatBySector == "json" || $inputFormatBySector == "") ? 'checked="checked"' : ''; // Save last checked or default if none given ?>
+                        >JSON<br/>
+
+                        <input type="radio" name="inputFormatBySector" value="xml"
+                        <?php echo ($inputFormatBySector == "xml") ? 'checked="checked"' : ''; // Save last checked ?>
+                        >XML<br/>
+
+                        <input type="radio" name="inputFormatBySector" value="csv"
+                        <?php echo ($inputFormatBySector == "csv") ? 'checked="checked"' : ''; // Save last checked ?>
+                        >CSV<br/>
+
+                        <input type="radio" name="inputFormatBySector" value="table"
+                        <?php echo ($inputFormatBySector == "table") ? 'checked="checked"' : ''; // Save last checked ?>
+                        >Table<br/>
+                        <br/>
+                        <input type="submit" name="submitSectorSummary" value="Submit">
                     </form>
                 </div>
             </div>
